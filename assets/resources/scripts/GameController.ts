4a8e65e4b6a5e26f5f4b9d5d2d7a44622fb7972b
faith.wang <wan199406@gmail.com>
2024-07-16 22:27:17 +0800

    add debug

--- assets/resources/scripts/GameController.ts
+++ assets/resources/scripts/GameController.ts
@@ -1,14 +1,19 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, EPhysics2DDrawFlags, EPhysicsDrawFlags, Node, PhysicsSystem, PhysicsSystem2D } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameController')
 export class GameController extends Component {
     start() {
-
+        this.debug();
     }
 
     update(deltaTime: number) {
         
+    }
+
+    debug() {
+        PhysicsSystem2D.instance.enable = true;
+        PhysicsSystem2D.instance.debugDrawFlags = EPhysics2DDrawFlags.All;
     }
 }
 
